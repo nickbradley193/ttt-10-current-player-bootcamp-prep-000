d02c10fc3e0d87cf2fbce664ebe57d0a88f88e6f
Nick Bradley <nick.bradley.193@pm.me>
2018-08-30 05:04:52 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  end
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  elsif
+    "O"
+  end
+end
